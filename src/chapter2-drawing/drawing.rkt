c379c75e2623a3ce86473ee592bf21e4aad96dc3
Niklas Larsson <niklas@mm.st>
2023-07-21 23:56:35 +0200

    Add scrolling

--- src/chapter2-drawing/drawing.rkt
+++ src/chapter2-drawing/drawing.rkt
@@ -6,15 +6,21 @@
 
 (define browser-window%
   (class frame%
+    (field (displ #f))
+    (define/public (load site)
+      (define body (get-page site))
+      (define txt (lex body))
+      (define l (layout txt))
+      (set! displ l))
     (super-new)))
 
 (define web-canvas%
   (class canvas%
-    (field (page #f))
-    (define/public (load site)
-      (define body (get-page site))
-      (define txt (lex body))
-      (set! page txt))
+    (field (scroll-x 0))
+    (define/override (on-scroll event)
+      (define pos (send event get-position))
+      (set! scroll-x pos)
+      (send this refresh))
     (super-new)))
 
 (define browser
@@ -49,21 +55,43 @@
               [else (emit ch)])
             (loop)))))
   (get-output-string output))
-  
+
+(struct display-list (items width height))
+(struct layout-item (x y content))
+
+(define (layout page)
+  (define hstep 18)
+  (define vstep 18)
+  (define hmax 800)
+  (let loop ((displist '()) (x 0) (y 0) (rest (string->list page)))
+    (if (null? rest)
+        (display-list (reverse displist) hmax y)
+        (let* ((new-x (if (< (+ x hstep) hmax) (+ x hstep) 0))
+               (new-y (if (> x new-x) (+ vstep y) y)))
+          (loop (cons (layout-item x y (car rest)) displist) new-x new-y (cdr rest))))))
+
+(define (draw displ dc offset)
+  (define vmax (display-list-height displ))
+  (define-values (w h) (send dc get-size))
+  (define top (* (- vmax h) (/ offset 100)))
+  (for ((item (display-list-items displ)))
+    (match item
+      ((layout-item x y (? char? ch))
+       (when (and (> y top) (< y (+ top h))) (send dc draw-text (string ch) x (- y top)))))))
+
 (define canvas
   (new web-canvas%
-   (parent browser)
-   (paint-callback
-    (λ (canvas dc)
-      (for ((ch (get-field page canvas)) (i (in-naturals)))
-        (let ((x (* 18 (remainder i 43)))
-              (y (* 18 (quotient i 43))))
-          (send dc draw-text (string ch) x y)))))))
+       (parent browser)
+       (style '(vscroll))
+       (paint-callback
+        (λ (canvas dc)
+          (draw (get-field displ browser) dc (get-field scroll-x canvas))))))
+
+(send canvas init-manual-scrollbars #f 100 4 4 0 0)
 
 (send browser show #t)
 
 
-
 (define test-url "https://browser.engineering/examples/xiyouji.html")
 
-(send canvas load test-url)
+(send browser load test-url)
