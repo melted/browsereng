c5c4ea81efbe8c1332b302eb2fa940f92fca1dba
Niklas Larsson <niklas@mm.st>
2023-07-22 17:43:42 +0200

    Zoom and resizing

--- src/chapter2-drawing/drawing.rkt
+++ src/chapter2-drawing/drawing.rkt
@@ -7,11 +7,21 @@
 (define browser-window%
   (class frame%
     (field (displ #f))
+    (field (text #f))
+    (field (text-size 15))
     (define/public (load site)
       (define body (get-page site))
       (define txt (lex body))
-      (define l (layout txt))
-      (set! displ l))
+      (set! text txt)
+      (define l (layout txt (send this get-width) text-size))
+      (set! displ l)
+      (send this refresh))
+    (define/override (on-size w h)
+      (relayout))
+    (define/public (relayout)
+      (when text
+        (set! displ (layout text (send this get-width) text-size))
+        (send this refresh)))
     (super-new)))
 
 (define web-canvas%
@@ -23,11 +33,16 @@
       (define key (send event get-key-code))
       (define pos (send this get-scroll-pos 'vertical))
       (define max-pos (send this get-scroll-range 'vertical))
+      (define parent (send this get-parent))
       (match key
         ('up (send this set-scroll-pos 'vertical (max (- pos 100) 0)))
         ('wheel-up (send this set-scroll-pos 'vertical (max (- pos 100) 0)))
         ('down (send this set-scroll-pos 'vertical (min (+ pos 100) max-pos)))
         ('wheel-down (send this set-scroll-pos 'vertical (min (+ pos 100) max-pos)))
+        (#\+ (set-field! text-size parent 36)
+             (send parent relayout))
+        (#\- (set-field! text-size parent 20)
+             (send parent relayout))
         (else (void)))
       (send this refresh))
     (super-new)))
@@ -65,16 +80,14 @@
             (loop)))))
   (get-output-string output))
 
-(struct display-list (items width height))
+(struct display-list (items width height font-size))
 (struct layout-item (x y content))
 
-(define (layout page)
-  (define hstep 18)
-  (define vstep 18)
-  (define hmax 800)
+(define (layout page hmax hstep)
+  (define vstep (inexact->exact (floor (* hstep 1.33))))
   (let loop ((displist '()) (x 0) (y 0) (rest (string->list page)))
     (if (null? rest)
-        (display-list (reverse displist) hmax y)
+        (display-list (reverse displist) hmax y hstep)
         (let* ((new-x (if (< (+ x hstep) hmax) (+ x hstep) 0))
                (new-y (if (> x new-x) (+ vstep y) y))
                (ch (car rest)))
@@ -82,15 +95,39 @@
             ((char=? ch #\newline) (loop displist 0 (+ 20 y) (cdr rest)))
             (else (loop (cons (layout-item x y (car rest)) displist) new-x new-y (cdr rest))))))))
 
+(define (get-replacement-image ch)
+  (if (char>? ch #\U10000)
+      (begin   
+        (let* ((hex (number->string (char->integer ch) 16))
+               (file (format  "D:/Niklas/src/browsereng/resources/openmoji/~a.png"
+                              (string-upcase hex))))
+        (if (file-exists? file)
+            (call-with-input-file file read-bitmap)
+            #f)))
+      #f))
+  
+  
+
 (define (draw displ dc offset)
   (define vmax (display-list-height displ))
+  (define text-size (display-list-font-size displ))
   (define-values (w h) (send dc get-size))
   (define top offset)
+  (define font (make-font #:size (floor (* text-size 0.75))))
+  (send dc set-font font)
   (for ((item (display-list-items displ)))
     (match item
       ((layout-item x y (? char? ch))
        (cond
-         ((and (> y (- top 20)) (< y (+ top h 20))) (send dc draw-text (string ch) x (- y top)))
+         ((and (> y (- top 20)) (< y (+ top h 20)))
+          (let ((repl (get-replacement-image ch)))
+            (if repl
+                (begin
+                  (let-values (((old-w old-h) (send dc get-scale)))
+                    (send dc set-scale 0.25 0.25)
+                    (send dc draw-bitmap repl (* 4 x) (* 4 (- y top)))
+                    (send dc set-scale old-w old-h)))
+                (send dc draw-text (string ch) x (- y top)))))
          (else (void)))))))
 
 (define canvas
@@ -111,5 +148,6 @@
 
 
 (define test-url "https://browser.engineering/examples/xiyouji.html")
+(define test-url2 "file:///D:/Niklas/src/browsereng/test/default.html")
 
 (send browser load test-url)
