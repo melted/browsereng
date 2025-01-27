61bb6c48ebb04c87aca49b6c8979a84623ce09eb
Niklas Larsson <niklas@mm.st>
2025-01-28 00:12:13 +0100

    fix entity reading

--- src/lib/utils.rkt
+++ src/lib/utils.rkt
@@ -1,31 +1,29 @@
 #lang racket
-
+(require "entities.rkt")
 (provide read-until read-entity convert-entity string-find string-cut)
 
 
 (define (read-until port end-char)
-  (let loop ([chars '()])
+  (define str (open-output-string))
+  (let loop ()
     (let ([ch (read-char port)])
       (cond
         ((eof-object? ch) #f)
         ((char=? end-char ch)
-          (list->string (reverse chars)))
-        (else (loop (cons ch chars)))))))
-
+          (write-char ch str)
+          (get-output-string str))
+        (else (write-char ch str)
+              (loop))))))
 
 (define (convert-entity entity)
   (cond
     [(string-prefix? entity "#x") (integer->char (string->number (substring entity 2) 16))]
     [(string-prefix? entity "#") (integer->char (string->number (substring entity 1)))]
-    [else
-     (match entity
-       ["gt" #\>]
-       ["lt" #\<]
-       ["shy" #\u00ad]
-       [else (error "non-supported entity")])]))
+    [else (hash-ref named-entities entity entity)]))
 
 (define (read-entity port)
-  (let ([entity (string-downcase (read-until port #\;))]) (convert-entity entity)))
+  (let ([entity (read-until port #\;)])
+    (convert-entity entity)))
 
 (define (string-find str contained)
   (regexp-match-positions (regexp-quote contained) str))
